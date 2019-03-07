3398b0bf6cefbd8e77cca06175f9d534b3c1f9b2
Thijs <22070@ma-web.nl>
2019-03-07 12:34:04 +0100

    added fields and field occupier objects

---
+++ Assets/Scripts/Field/Field.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour {
+
+    [SerializeField]
+    private List<Field> nextFields;
+    [SerializeField]
+    private List<Field> previousFields;  
+    public FieldOccupier onField;
+}
---
+++ Assets/Scripts/Field/FieldOccupier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FieldOccupierType {
+    EMPTY,
+    PLAYER,
+    BARRICADE
+}
+
+public class FieldOccupier : MonoBehaviour {
+
+    private Field startField;
+
+    [SerializeField]
+    private Field currentField;
+
+    private FieldOccupierType myType;
+
+    /// <summary> all fieldOccupiers must implement this function to move to fields </summary>
+    public abstract void MoveToField(Field nextField, Action callback);
+}
