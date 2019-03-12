2019fd8403101fb513f7a4e57d1d2c7b28c244d3
Niels van der Marel <nielsvdmarel@gmail.com>
2019-03-12 10:40:16 +0100

    Final movement and cleanup. added util script.

--- Assets/Scripts/Player/Pawn.cs
+++ Assets/Scripts/Player/Pawn.cs
@@ -3,52 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : FieldOccupier {
+public class Pawn : FieldOccupier {
     [SerializeField]
     private float movementSpeed;
     [SerializeField]
     private bool moving;
     [SerializeField]
     private Field nextField;
-    private float step = .1f;
+    [SerializeField]
+    private float step = 2f;
     private float yOffset = .55f;
     void Start() {
 
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
-            MoveToField(nextField, null);
-        }
-        SelectNextField();
-    }
-
+    /// <summary>starts a coroutine that makes the GameObject move to nextField.</summary>
     public override void MoveToField(Field nextField, Action callback) {
         StartCoroutine(MoveStep(nextField, callback));
     }
 
+    /// <summary>coroutine for iterating current GameObject position and rotation to nextField position and moving direction.</summary>
     private IEnumerator MoveStep(Field nextField, Action callback) {
         Vector3 target = new Vector3(nextField.transform.position.x, nextField.transform.position.y + yOffset, nextField.transform.position.z);
         do {
-            transform.position =  Vector3.MoveTowards(transform.position, target, step);
+            transform.position =  Vector3.MoveTowards(transform.position, target, step * Time.deltaTime);
             transform.LookAt(target);
             yield return null;
         } while (transform.position != target);
         callback?.Invoke();
         currentField = nextField;
         transform.eulerAngles = Vector3.zero;
-    }
-
-    private void SelectNextField() {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit)) {
-                Debug.DrawLine(Camera.main.transform.position, hit.transform.position, Color.red);
-                if (hit.transform.gameObject.GetComponent<Field>() != null) {
-                    nextField = hit.transform.gameObject.GetComponent<Field>();
-                } 
-            }
-        }
     }
 }
