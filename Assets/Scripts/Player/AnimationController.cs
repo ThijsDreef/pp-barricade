c5fe1c299c015376d57b73f6e4aad417528644be
Niels van der Marel <nielsvdmarel@gmail.com>
2019-03-12 15:21:30 +0100

    Movement now sync with animations and new prefab/model

---
+++ Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    private Animator animator;
+    private Pawn pawn;
+    void Start() {
+        animator = GetComponent<Animator>();
+        pawn = GetComponent<Pawn>();
+        pawn.StartMoving += StartmoveAnim;
+        pawn.StopMoving += StartIdleAnim;
+    }
+
+    void StartmoveAnim() {
+        animator.SetTrigger("Walk");
+    }
+
+    void StartIdleAnim() {
+        animator.SetTrigger("Idle");
+    }
+
+    void StartAttackAnim() {
+        animator.SetTrigger("Attack");
+    }
+}
