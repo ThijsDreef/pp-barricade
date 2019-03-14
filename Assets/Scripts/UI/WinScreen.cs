def0be5f46dc7520f0a2074315d32c474513cbc3
Rico Evers <wiziwizi@github.com>
2019-03-14 19:01:19 +0100

    Blur effect and win screen added

---
+++ Assets/Scripts/UI/WinScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    public Text winText;
+
+    private void Start() {
+        SoundManager.Instance.PlaySound("Victory");
+        print("Thijs connect deze functie even met de game controller");
+        //winText.text = "Player " + GameController.Instance.currentPlayer + " Wins!";
+    }
+}
